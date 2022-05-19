dc47e6ac0a74a4ace5561cd0fe6f8c2d5e31815b
insach67 <insach67@gmail.com>
2022-05-19 11:26:42 +0530

    first commit

---
+++ src/components/Text.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Input.css';
+
+function Text() {
+  return (
+    <div className='textform'>
+        <h2>EMPLOYEE FEEDBACK FORM</h2>
+        </div>
+  )
+}
+
+export default Text
